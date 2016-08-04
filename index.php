038115a9b8a4503a0e4d48c29f252456a1265161
lsg9311 <crit9411@naver.com>
2016-08-04 23:11:13 +0900

    install bootstrap

---
+++ index.php
@@ -0,0 +1,10 @@
+<html>
+	<head>
+		<link href="/css/bootstrap.css" rel="stylesheet"/>
+		<link href="/css/bootstrap-theme.css" rel="stylesheet"/>
+		<script src="/js/bootstrap.css"/>
+	</head>
+<body>
+	<button class="btn btn-primary">TEST BUTTON</button>
+</body>
+</html>
