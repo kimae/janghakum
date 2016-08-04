acc0ffbc6b99a3e6f688a92e90957b563052025c
lsg9311 <crit9411@naver.com>
2016-08-04 23:23:18 +0900

    test v0.0.1

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
 <html>
 	<head>
-		<link href="/css/bootstrap.css" rel="stylesheet"/>
-		<link href="/css/bootstrap-theme.css" rel="stylesheet"/>
-		<script src="/js/bootstrap.css"/>
+		<link href="./lib/bootstrap/css/bootstrap.css" rel="stylesheet"/>
+		<link href="./lib/bootstrap/css/bootstrap-theme.css" rel="stylesheet"/>
+		<script src="./lib/bootstrap/js/bootstrap.js"></script>
 	</head>
-<body>
-	<button class="btn btn-primary">TEST BUTTON</button>
-</body>
+
+	<body>
+		<button class="btn btn-primary">TEST BUTTON</button>
+	</body>
 </html>
